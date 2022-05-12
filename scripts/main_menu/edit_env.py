1a4237dd3c918ef141da11ca917aea8c16bef8f9
dolag233 <1284773289@qq.com>
2022-05-12 11:44:40 +0800

    change json open method
    change to os.startfile

--- scripts/main_menu/edit_env.py
+++ scripts/main_menu/edit_env.py
@@ -8,10 +8,7 @@ from error.error_report import displayError
 pref_dir = os.environ["HOUDINI_USER_PREF_DIR"]
 ENV_JSON_PATH = pref_dir + r"/packages/DolagPlugin.json"
 if os.path.isfile(ENV_JSON_PATH):
-    if "Windows" == os.environ["HOUDINI_OS"]:
-        subprocess.call(["notepad", ENV_JSON_PATH])
-    else:
-        subprocess.call(["nano", ENV_JSON_PATH])
+    os.startfile(ENV_JSON_PATH)
 
 else:
     displayError("env_json")
