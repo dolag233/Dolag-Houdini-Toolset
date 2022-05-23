28f0558bb80e7f0849113404b3f954b18f6b5873
dolag233 <1284773289@qq.com>
2022-05-24 01:09:18 +0800

    [Feat + Fix] Mend get node function
    Fix: get nothing when input a hou.Node instance
    Feat: add the second parameter to set the node type

--- python/python2.7libs/Dolag/_nodeutils.py
+++ python/python2.7libs/Dolag/_nodeutils.py
@@ -2,15 +2,20 @@ import hou
 
 
 # get node from str or hou.Node
-def getNode(node_desc):
+def getNode(node_desc, node_type=hou.Node):
     node = None
     # if input node path
     if isinstance(node_desc, str):
         node = hou.node(node_desc)
 
-    # if input is a node instance
-    elif isinstance(node, hou.Node):
+    # if node_type is not tuple
+    elif (not isinstance(node_type, tuple)) and isinstance(node_desc, node_type):
         node = node_desc
+
+    elif isinstance(node_type, tuple):
+        for nt in node_type:
+            if isinstance(node_desc, nt):
+                node = node_desc
 
     else:
         pass
