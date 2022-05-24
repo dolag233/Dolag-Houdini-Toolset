1a26d6654516fecd119f93314d3b8b58cfd0c8d8
dolag233 <1284773289@qq.com>
2022-05-24 15:31:55 +0800

    [Feat] Increase file name

--- python/python2.7libs/Dolag/_pathutils.py
+++ python/python2.7libs/Dolag/_pathutils.py
@@ -65,6 +65,62 @@ def getRelativePath(src, dest):
     return res_str
 
 
+# increase file name
+# e.g. dolag -> dolag_0 | dolag_999 -> dolag_1000
+def increaseFileName(file_name, has_suffix=True):
+    suffix = ''
+
+    if has_suffix:
+        suffix = '.' + str(file_name.split('.')[-1:][0])
+        file_name = '.'.join(file_name.split('.')[:-1])
+
+    # get version number
+    file_name_rev = file_name[::-1]
+    version = []
+    version_start = 0
+    for c in file_name_rev:
+        if '9' >= c >= '0':
+            version.append(int(c))
+            version_start += 1
+        else:
+            break
+
+    # for "abc123", the version_start is 3
+    version_start = len(file_name) - version_start
+
+    if len(version) > 0:
+        # get reversed version
+        version = version[::-1]
+        carry = [0 for i in range(len(version) + 1)]
+        for i in range(len(version)):
+            dig = version[i]
+            dig += 1 if i == 0 else 0
+            # if carry
+            dig_sum = dig + carry[i]
+            if dig_sum >= 10:
+                version[i] = dig_sum % 10
+                carry[i + 1] = 1
+            # else not carry
+            else:
+                version[i] = dig_sum
+
+        # if version == something like 999999
+        if carry[-1] == 1:
+            version.append(1)
+        version.reverse()
+
+    else:
+        pass
+
+    # convert to digital version
+    version = ''.join(list(map(str, version)))
+    if len(version) == 0:
+        version = '_0'
+
+    file_name = file_name[:version_start] + version + suffix
+    return file_name
+
+
 # get hda library path
 # return None if no otl file
 def getHdaLibraryPath(hda_name):
