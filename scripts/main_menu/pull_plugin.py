5e779268fd9f6b09b27306bce8aa02a4e5f6ff01
dolag <dolag233@gmail.com>
2023-07-04 10:36:33 +0800

    [Feat] Pull plugin

--- scripts/main_menu/pull_plugin.py
+++ scripts/main_menu/pull_plugin.py
@@ -14,15 +14,17 @@ shell_path = hou_path + "/bin/hcmd.exe"
 
 print("Start pulling DolagPlugin...")
 
-subprocess.call((shell_path, '/c', r'cd "{}" && git pull -b dev'.format(dolag_path)), stderr=sys.stderr, stdout=sys.stdout, shell=True)
+driver = os.path.splitdrive(dolag_path)[0]
 
+subprocess.call((shell_path, '/c', r'{} && cd "{}" && git pull'.format(driver, dolag_path)), stderr=sys.stderr, stdout=sys.stdout, shell=True)
+print(r'{} && cd "{}" && git pull'.format(driver, dolag_path))
 new_json_path = os.path.abspath(dolag_path + "DolagPlugin.json")
 json_path = os.path.abspath(dolag_path + "../../packages/DolagPlugin.json")
 
 if os.path.exists(new_json_path):
     shutil.copy(new_json_path, json_path)
+    print("End pulling!\nAutomatically copied DolagPlugin.json to houdini X.Y/packages for you")
 
-print("End pulling!\nAutomatically copied DolagPlugin.json to houdini X.Y/packages for you")
-
-
+else:
+    print("Pulling failed.")
 
