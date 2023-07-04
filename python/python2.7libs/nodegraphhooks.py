8051e3390d1114b7df2c7514707723c58e787fb7
dolag <dolag233@gmail.com>
2023-07-04 15:45:30 +0800

    [Feat] Change double click delta time

--- python/python2.7libs/nodegraphhooks.py
+++ python/python2.7libs/nodegraphhooks.py
@@ -9,11 +9,13 @@ from nodegraphconsole import KeyEventHandler
 
 __last_mouse_state = False
 __last_mouse_click_time = time.time()
+__mouse_double_click_delta_time = 0.4
 
 # Ctrl + Space to summon hot console
 def createEventHandler(uievent, pending_actions):
     global __last_mouse_state
     global __last_mouse_click_time
+    global __mouse_double_click_delta_time
     if isinstance(uievent, KeyboardEvent) and \
             uievent.eventtype == 'keyhit' and \
             uievent.key == 'Ctrl+Space':
@@ -31,7 +33,7 @@ def createEventHandler(uievent, pending_actions):
         cur_time = time.time()
 
         ## time out
-        if time.time() - __last_mouse_click_time > 0.3:
+        if time.time() - __last_mouse_click_time > __mouse_double_click_delta_time:
             __last_mouse_state = False
             __last_mouse_click_time = 0
 
