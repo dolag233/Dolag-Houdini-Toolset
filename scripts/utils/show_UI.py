672d6984236cac04b9785e7d8eca70384aa2b141
dolag <dolag233@gmail.com>
2024-01-14 14:10:29 +0800

    [Feat] Support standalone qt window

--- scripts/utils/show_UI.py
+++ scripts/utils/show_UI.py
@@ -7,7 +7,9 @@ try:
 except Exception as e:
     raise
 
+standalone_panels = []
 
+# this will block other ui thread
 def showUI(func, *args):
     """
     Show UI instance
@@ -22,3 +24,20 @@ def showUI(func, *args):
     dialog.show()
     dialog.exec_()
     return dialog
+
+def showUIStandalone(func, *args):
+    """
+    Show UI instance
+    """
+    app = QtGui.QApplication.instance()
+
+    if not app:
+        app = QtGui.QApplication([APP])
+
+    dialog = func(*args)
+    dialog.raise_()
+    dialog.show()
+    global standalone_panels
+    standalone_panels.append(dialog)
+    return dialog
+
