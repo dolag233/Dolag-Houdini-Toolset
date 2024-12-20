c876f9377cbe7dffc7f14f2cee5d5c80328ae906
dolag <dolag233@gmail.com>
2024-12-21 02:39:03 +0800

    Squashed commit of the following:
    commit ac340421940149575b9bdc25a390d297df89ef5b
    Author: dolag <dolag233@gmail.com>
    Date:   Sat Nov 9 00:27:20 2024 +0800
    
        [Feat] Scale a geo to match the volume of second input or specific value
    
    commit 3ffef8e0090f3b3b10312652c7340c1ad7cb5220
    Author: dolag <dolag233@gmail.com>
    Date:   Fri Nov 8 11:54:46 2024 +0800
    
        [Feat] Volume connectivity
    
    commit 663b1fa27be1156835317d049efdb4122b318924
    Author: huangqingsong <huangqingsong@corp.netease.com>
    Date:   Thu Nov 7 00:16:23 2024 +0800
    
        [Feat] Unlock all child nodes and save as a copy
    
    commit 05feeacf739222b84bb65f4e73ff0a2240f01dfe
    Author: huangqingsong <huangqingsong@corp.netease.com>
    Date:   Thu Nov 7 00:15:26 2024 +0800
    
        [Feat] Load texture with alpha channel
    
    commit 196f92ca27e5f83e693891ee4510f3c7ab5df2e0
    Author: dolag <dolag233@gmail.com>
    Date:   Thu Aug 8 10:18:52 2024 +0800
    
        [Feat] Support only generate curves
    
    commit 128d21699a1b3b65264bc096a12fa12cd709df09
    Merge: 712fde7 0cde331
    Author: dolag <dolag233@gmail.com>
    Date:   Tue Jul 23 01:05:49 2024 +0800
    
        Merge branch 'dev' of https://github.com/dolag233/Dolag-Houdini-Toolset into dev
    
    commit 712fde76db465bfd3691d2243e3cb04b8c3d8963
    Author: dolag <dolag233@gmail.com>
    Date:   Mon Jul 22 09:35:23 2024 +0800
    
        [Fix] Fix the wrong result of curve distance
    
    commit dc64315c554116bf7d8422bbb633efc2a1b4e99c
    Author: dolag <dolag233@gmail.com>
    Date:   Sat Jul 20 15:07:00 2024 +0800
    
        [Feat] Support change curveu name and get accumulated distance
    
    commit 0cde331c5e567a02e62db3db3e0b0cd8544546b7
    Author: huangqingsong <huangqingsong@corp.netease.com>
    Date:   Tue Jul 2 21:53:59 2024 +0800
    
        [Feat] Create Unreal static mesh LOD
    
    commit b121faa9967b9c10da6dc330e137ace358c93e92
    Author: huangqingsong <huangqingsong@corp.netease.com>
    Date:   Thu Jun 20 15:21:35 2024 +0800
    
        [Fix] Fix path error
    
    commit b1fde8a7ad0c623787bb14ccaa943adbe64473e4
    Author: dolag <dolag233@gmail.com>
    Date:   Thu Feb 1 15:58:35 2024 +0800
    
        [Feat] Support noise remapping
    
    commit 64723b60600249d7635b2a5ef8ff388558bbfb8c
    Author: dolag <dolag233@gmail.com>
    Date:   Tue Jan 30 17:34:47 2024 +0800
    
        [Feat] Glow stickers
    
    commit d84792c0eff449d993dc559457ca21a14a5a29c6
    Author: dolag <dolag233@gmail.com>
    Date:   Mon Jan 29 15:57:17 2024 +0800
    
        [Feat] Support rotate camera
    
        And finally I found that, there has an official first-person navigation mode. Sad.
    
    commit a085ff297d983632797063c4464213ccc4b951a6
    Author: dolag <dolag233@gmail.com>
    Date:   Mon Jan 29 14:39:04 2024 +0800
    
        [Fix] Fix crashes when pressing middle wheel
    
    commit 1ba8308a3c5e2cfbd5cc38f856cbca1ea3e55fe8
    Author: dolag <dolag233@gmail.com>
    Date:   Mon Jan 29 14:10:32 2024 +0800
    
        [Fix] Fix crashes when pressing middle wheel
    
    commit 40f56752b83921d1cb262d57aa6714a51a42de4c
    Author: dolag <dolag233@gmail.com>
    Date:   Mon Jan 29 14:09:23 2024 +0800
    
        [Feat] Random cache name when created or loaded, avoid confliction
    
    commit 20de71fb13b67d18af23e4e421dbbd7fe5ce4c7d
    Author: dolag <dolag233@gmail.com>
    Date:   Mon Jan 29 11:52:10 2024 +0800
    
        [Feat] Support reveal volume attribute by the second input
    
    commit ccb624e455a2729b7bdd154eef608993cadf6b93
    Author: dolag <dolag233@gmail.com>
    Date:   Mon Jan 29 11:07:41 2024 +0800
    
        [Feat] Scale a geometry to or from specific volume
    
    commit 135e6afeade33f1bf0532a3cb2ffdac649574bd5
    Author: dolag <dolag233@gmail.com>
    Date:   Fri Jan 26 18:12:00 2024 +0800
    
        [Feat] UE-like scene walker using viewer state

---
+++ viewer_states/viewer_wanderer.py
@@ -0,0 +1,152 @@
+"""
+State:          Viewer Wanderer
+State type:     viewer_wanderer
+Description:    wander through the viewer
+Author:         Dolag
+Date Created:   January 26, 2024 - 14:23:49
+"""
+
+# Usage: Select the node and hit enter in the viewer.
+
+import hou
+import viewerstate.utils as su
+import stateutils
+
+
+class State(object):
+    speed = 0.15
+    speed_step = 0.05
+    rotate_matrix = hou.Matrix3([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    rotate_mouse_pos = hou.Vector2(0, 0)
+    rotate = False
+    MSG = "Press Ctrl + WSADEQ to move the camera\nScroll wheel to set move speed. Current speed : {}"
+    def __init__(self, state_name, scene_viewer):
+        self.state_name = state_name
+        self.scene_viewer = scene_viewer
+
+    def displayPrompt(self):
+        self.scene_viewer.setPromptMessage(State.MSG.format(self.speed))
+
+    def onEnter(self, kwargs):
+        self.displayPrompt()
+
+    def onResume(self, kwargs):
+        self.displayPrompt()
+
+    def onKeyEvent(self, kwargs):
+        ui_device = kwargs["ui_event"].device()
+        #  wander
+        if ui_device.isKeyDown():
+            key = ui_device.keyString()
+            if key in ('Ctrl+w', 'Ctrl+s', 'Ctrl+a', 'Ctrl+d', 'Ctrl+e', 'Ctrl+q'):
+                viewer = stateutils.findSceneViewer()
+                if viewer:
+                    vp = viewer.curViewport()
+                    camera = vp.defaultCamera()
+                    forward = hou.Vector3(0, 0, -1)  # camera forward
+                    #  forward = forward * camera.rotation()  # no, camera.rotation() rotates a vector into view space
+                    forward = forward * camera.rotation().inverted()
+                    forward = forward.normalized()
+                    #  camera.pivot() is the transform centroid in world space
+                    #  camera.translate() is the translation relative to the pivot in view space
+                    #  camera.rotate() is the rotation relative to the pivot
+                    #  camera.translate() * camera.rotation().inverted() is the camera position in world space
+                    move_step = self.speed
+                    if key == 'Ctrl+w':
+                        camera.setTranslation(tuple(a + b for a, b in zip(camera.translation(), [0, 0, -move_step])))
+                    elif key == 'Ctrl+s':
+                        camera.setTranslation(tuple(a + b for a, b in zip(camera.translation(), [0, 0, move_step])))
+                    elif key == 'Ctrl+a':
+                        camera.setTranslation(tuple(a + b for a, b in zip(camera.translation(), [-move_step / camera.orthoWidth(), 0, 0])))
+                    elif key == 'Ctrl+d':
+                        camera.setTranslation(tuple(a + b for a, b in zip(camera.translation(), [move_step / camera.orthoWidth(), 0, 0])))
+                    elif key == 'Ctrl+e':
+                        camera.setTranslation(tuple(a + b for a, b in zip(camera.translation(), [0, move_step / (camera.orthoWidth() / camera.aspectRatio()), 0])))
+                    elif key == 'Ctrl+q':
+                        camera.setTranslation(tuple(a + b for a, b in zip(camera.translation(), [0, -move_step / (camera.orthoWidth() / camera.aspectRatio()), 0])))
+                    self.displayPrompt()
+
+                return True
+
+    def onMouseEvent(self, kwargs):
+        ui_device = kwargs["ui_event"].device()
+        if ui_device.isMiddleButton():
+            #  rotate camera
+            viewer = stateutils.findSceneViewer()
+            if viewer:
+                vp = viewer.curViewport()
+                camera = vp.defaultCamera()
+                if not self.rotate:
+                    #  record the init mouse pos
+                    self.rotate_mouse_pos = hou.Vector2(ui_device.mouseX() / vp.size()[2] - vp.size()[0],
+                                                        ui_device.mouseY() / vp.size()[3] - vp.size()[1])
+                    self.rotate_matrix = camera.rotation()
+                    self.rotate = True
+
+                forward = hou.Vector3((0, 0, -1)) * camera.rotation()
+                center = hou.Vector3(camera.pivot()) + hou.Vector3(camera.translation()) * camera.rotation().inverted()
+                forward = center - hou.Vector3(camera.pivot())
+                forward = forward.normalized()
+                #  camera.setPivot(tuple(center))
+                #  camera.setTranslation((0, 0, 0))
+                mouse_pos = hou.Vector2(ui_device.mouseX() / vp.size()[2] - vp.size()[0],
+                                               ui_device.mouseY() / vp.size()[3] - vp.size()[1])
+                delta_pos = mouse_pos - self.rotate_mouse_pos
+
+                # 假设你已经有了一个yaw和pitch的角度（以度为单位）
+                yaw = delta_pos[1] * 90  # Yaw角度
+                pitch = delta_pos[0] * 90  # Pitch角度
+
+                # 使用hou.hmath.buildRotate函数来构造旋转矩阵
+                rotation_matrix = hou.hmath.buildRotate((-yaw, 0, 0))
+                rotation_matrix1 = hou.hmath.buildRotate((0, pitch, 0))
+                rotation = self.rotate_matrix * rotation_matrix.extractRotationMatrix3() * rotation_matrix1.extractRotationMatrix3()
+                camera.setRotation(rotation)
+
+                return True
+
+        else:
+            self.rotate = False
+
+        return False
+
+    def onMouseWheelEvent(self, kwargs):
+        ui_device = kwargs["ui_event"].device()
+        #  change speed
+        if ui_device.mouseWheel() != 0:
+            self.speed = self.speed + ui_device.mouseWheel() * self.speed_step
+            self.displayPrompt()
+            return True
+
+        return False
+
+    def onKeyTransitEvent(self, kwargs):
+        ui_device = kwargs["ui_event"].device()
+        if hou.applicationVersion()[0] >= 20 and hou.applicationVersion()[1] >= 0:  # middle button api is added since 20.0
+            if ui_device.isMiddleButton() and not self.rotate:
+                viewer = stateutils.findSceneViewer()
+                vp = viewer.curViewport()
+                camera = vp.defaultCamera()
+                #  record the init mouse pos
+                self.rotate_mouse_pos = hou.Vector2(ui_device.mouseX() / vp.size()[2] - vp.size()[0],
+                                                   ui_device.mouseY() / vp.size()[3] - vp.size()[1])
+                self.rotate = True
+            elif ui_device.isMiddleButtonReleased():
+                self.rotate = False
+
+        self.displayPrompt()
+
+
+def createViewerStateTemplate():
+    """ Mandatory entry point to create and return the viewer state 
+        template to register. """
+
+    state_typename = "dolag::viewer_wanderer"
+    state_label = "Viewer Wanderer"
+    state_cat = hou.sopNodeTypeCategory()
+
+    template = hou.ViewerStateTemplate(state_typename, state_label, state_cat)
+    template.bindFactory(State)
+
+    return template
+
